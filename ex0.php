614bcc7c573e14f8c0fcb34b766db203785ad645
gorizon35 <hugo.elleouet@live.fr>
2018-02-21 17:29:03 +0100

    debut tp1

--- ex0.php
+++ ex0.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,8 +6,10 @@
 <title>Hello</title>
 </head>
 <body>
+<a href="/tp/ex0.php?message=Hello">Afficher Hello</a>
+
 <?php
-echo("Hello world !");
+echo "Vous êtes sur la page n° ".$_GET["page"];
 ?>
 </body>
 </html>
